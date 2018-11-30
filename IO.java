deb27fa69103345afd4f2dd90e7fc8568cb74db6
Erick Vaquero <evaquero6394@gmail.com>
2018-11-30 17:49:47 -0600

    Added IO.java

---
+++ IO.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class IO
+{
+    public static final Text = 0;
+    public static final GUI_1 = 1;
+    public static final GUI_2 = 2;
+    public static final GUI_3 = 3;
+
+    public void display(String s)
+    {
+
+    }
+
+    public String getLine(void)
+    {
+
+    }
+
+    public void selectInterface(int GUI)
+    {
+
+    }
+
+
+}//end public class IO
